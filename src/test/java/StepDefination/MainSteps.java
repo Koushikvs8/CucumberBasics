cb2ed2fc959c164ea94b743ea05cd391bb086f8e
Koushikvs8 <koushikvittalshet@gmail.com>
2024-11-05 15:33:17 +0530

    RegEX concept

--- src/test/java/StepDefination/MainSteps.java
+++ src/test/java/StepDefination/MainSteps.java
@@ -1,5 +1,7 @@
 package StepDefination;
 
+import java.util.List;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,10 +13,33 @@ public class MainSteps {
 		System.out.println("user on NetBanking landng page");
 	    
 	}
-	@When("User login in to application with {string} and password {string}")
+	@Given("user on guru99  application")
+	public void user_on_guru99_application() {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println("user on guru99  application");
+	}
+	@When("User login in to application")
+	public void user_login_in_to_application(List<String> data) {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+	    // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+	    // Double, Byte, Short, Long, BigInteger or BigDecimal.
+	    //
+	    // For other transformations you can register a DataTableType.
+		for(String data1 :data) {System.out.println(data1);}
+		
+	    
+	}
+	@When("^User login in to application with (.+) and password (.+)$")
 	public void user_login_in_to_application_with_and_password(String string, String string2) {
 		System.out.println(string + " "+string2);
 	    // Write code here that turns the phrase above into concrete actions
+	}
+	@When("User login in to application with {string} and password h  {string} combination")
+	public void user_login_in_to_application_with_and_password_combination(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+		System.out.println(string + " "+string2);
 	}
 	@Then("Home page is displayes")
 	public void home_page_is_displayes() {
--- src/test/java/features/first.feature
+++ src/test/java/features/first.feature
@@ -14,6 +14,31 @@ And Cards are displayed
  Scenario: Admin page default login
 
 Given  user on NetBanking landng page
-When User login in to application with "user" and password "123d4"
+When User login in to application with user and password 123d4
+Then Home page is displayes 
+And Cards are displayed
+
+ Scenario Outline: Admin page default login
+
+Given  user on NetBanking landng page
+When User login in to application with "<Username>" and password  "<password>"
+Then Home page is displayes 
+And Cards are displayed
+
+
+Examples:
+|Username|password|
+|koushik|4566666|
+|swathi|456666ss6|
+|shiva|45665ff666|
+
+ Scenario: Admin page default login
+
+Given  user on guru99  application
+When User login in to application 
+|koushik|
+|shet|
+|567890|
+|india|
 Then Home page is displayes 
 And Cards are displayed
